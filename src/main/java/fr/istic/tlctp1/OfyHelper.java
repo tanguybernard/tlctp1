03e50198f92604a54748925645b65698580bc54f
ramage <ramageromain@gmail.com>
2016-01-22 10:44:24 +0100

    mise en place publicité

--- src/main/java/fr/istic/tlctp1/OfyHelper.java
+++ src/main/java/fr/istic/tlctp1/OfyHelper.java
@@ -15,8 +15,7 @@ public class OfyHelper implements ServletContextListener {
     public void contextInitialized(ServletContextEvent event) {
         // This will be invoked as part of a warmup request, or the first user request if no warmup
         // request.
-        ObjectifyService.register(Guestbook.class);
-        ObjectifyService.register(Greeting.class);
+        ObjectifyService.register(Advertisement.class);
     }
 
     public void contextDestroyed(ServletContextEvent event) {
