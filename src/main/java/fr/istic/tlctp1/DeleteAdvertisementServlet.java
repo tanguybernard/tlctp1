5e80158f232d26684e8b62f04786dbfaadde73e9
ramage <ramageromain@gmail.com>
2016-02-08 17:22:20 +0100

    suppresion d'une pblicité

---
+++ src/main/java/fr/istic/tlctp1/DeleteAdvertisementServlet.java
@@ -0,0 +1,21 @@
+package fr.istic.tlctp1;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.googlecode.objectify.ObjectifyService;
+
+public class DeleteAdvertisementServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectifyService.ofy().delete().entities(req.getParameter("id"));
+        resp.sendRedirect("/index.jsp");
+	}
+}
